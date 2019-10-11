153b2b2ad4fbde31fc0fb841a6e99e54ca808e05
Alexander Wilkens <alexander.wilkens96@gmail.com>
2019-10-11 20:32:36 +0200

    Add source_ref to new expression parsing

--- src/parse/Expression.cc
+++ src/parse/Expression.cc
@@ -11,10 +11,17 @@
 #include "ast/VariableExpression.hh"
 
 ptr_t<Expression> Parser::parse_expression(TokenBuffer& tokens) {
-    return parse_assign(tokens);
+    auto begin = tokens.top_iterator();
+    auto expression = parse_assign(tokens);
+    auto end = tokens.top_iterator();
+
+    expression->source_ref.begin = begin;
+    expression->source_ref.end = end;
+    return expression;
 }
 
 ptr_t<Expression> Parser::parse_assign(TokenBuffer& tokens) {
+    auto begin = tokens.top_iterator();
     ptr_t<Expression> first = parse_compare(tokens);
 
     while (tokens.eat(ASSIGN)) {
@@ -23,10 +30,15 @@ ptr_t<Expression> Parser::parse_assign(TokenBuffer& tokens) {
         first = std::make_unique<AssignExpression>(first, op, second);
     }
 
+    auto end = tokens.top_iterator();
+    first->source_ref.begin = begin;
+    first->source_ref.end = end;
+
     return first;
 }
 
 ptr_t<Expression> Parser::parse_compare(TokenBuffer& tokens) {
+    auto begin = tokens.top_iterator();
     ptr_t<Expression> first = parse_add_sub(tokens);
 
     while (tokens.eat({ GT, GTEQ, LT, LTEQ, EQ, NOT_EQ })) {
@@ -35,10 +47,16 @@ ptr_t<Expression> Parser::parse_compare(TokenBuffer& tokens) {
         first = std::make_unique<BinaryExpression>(first, op, second);
     }
 
+    auto end = tokens.top_iterator();
+
+    first->source_ref.begin = begin;
+    first->source_ref.end = end;
+
     return first;
 }
 
 ptr_t<Expression> Parser::parse_add_sub(TokenBuffer& tokens) {
+    auto begin = tokens.top_iterator();
     ptr_t<Expression> first = parse_mult_div(tokens);
 
     while (tokens.eat({ PLUS, MINUS })) {
@@ -47,10 +65,15 @@ ptr_t<Expression> Parser::parse_add_sub(TokenBuffer& tokens) {
         first = std::make_unique<BinaryExpression>(first, op, second);
     }
 
+    auto end = tokens.top_iterator();
+    first->source_ref.begin = begin;
+    first->source_ref.end = end;
+
     return first;
 }
 
 ptr_t<Expression> Parser::parse_mult_div(TokenBuffer& tokens) {
+    auto begin = tokens.top_iterator();
     ptr_t<Expression> first = parse_unary(tokens);
 
     while (tokens.eat({ STAR, SLASH })) {
@@ -59,23 +82,40 @@ ptr_t<Expression> Parser::parse_mult_div(TokenBuffer& tokens) {
         first = std::make_unique<BinaryExpression>(first, op, second);
     }
 
+    auto end = tokens.top_iterator();
+    first->source_ref.begin = begin;
+    first->source_ref.end = end;
+
     return first;
 }
 
 ptr_t<Expression> Parser::parse_unary(TokenBuffer& tokens) {
+    auto begin = tokens.top_iterator();
+
+    ptr_t<Expression> expression;
+
     if (tokens.top().type == MINUS) {
         // @REFACTOR: We should probably have like parse_operator
         // instead...
         ptr_t<Operator> op = std::make_unique<Operator>(tokens.pop());
         ptr_t<Expression> right = parse_unary(tokens);
-        return std::make_unique<UnaryExpression>(op, right);
+        expression = std::make_unique<UnaryExpression>(op, right);
+    } else {
+        expression = Parser::parse_primary(tokens);
     }
 
-    return Parser::parse_primary(tokens);
+    auto end = tokens.top_iterator();
+
+    expression->source_ref.begin = begin;
+    expression->source_ref.end = end;
+
+    return expression;
 }
 
 ptr_t<Expression> Parser::parse_primary(TokenBuffer& tokens) {
     ptr_t<Expression> primary;
+
+    auto begin = tokens.top_iterator();
 
     Token current = tokens.pop();
 
@@ -92,18 +132,34 @@ ptr_t<Expression> Parser::parse_primary(TokenBuffer& tokens) {
         }
     } else {
         // @TODO: Throw parse error
-        throw 1;
+        throw std::runtime_error("Unexpected primary expression" +
+                                 current.to_string());
     }
+
+    auto end = tokens.top_iterator();
+    primary->source_ref.begin = begin;
+    primary->source_ref.end = end;
 
     return primary;
 }
 
 ptr_t<FunctionCall> Parser::parse_function_call(TokenBuffer& tokens) {
+    // Need the identifier aswell
+    auto begin = tokens.top_iterator() - 1;
+
     Token identifier = tokens.previous();
     // Identifier already eaten
     if (tokens.top().type != LPARENS) return nullptr;
 
     ptr_t<ArgumentList> argument_list = parse_argument_list(tokens);
 
-    return std::make_unique<FunctionCall>(identifier, argument_list);
+    auto end = tokens.top_iterator();
+
+    auto function_call =
+        std::make_unique<FunctionCall>(identifier, argument_list);
+
+    function_call->source_ref.begin = begin;
+    function_call->source_ref.end = end;
+
+    return function_call;
 }
