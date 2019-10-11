8445fe20ce2670ddbe85727c9d9640cb98f1a846
Alexander Wilkens <alexander.wilkens96@gmail.com>
2019-10-11 20:49:24 +0200

    Semantics for assign expression

--- src/ast/AssignExpression.cc
+++ src/ast/AssignExpression.cc
@@ -1,3 +1,20 @@
 #include "AssignExpression.hh"
+#include "VariableDecl.hh"
+#include "VariableExpression.hh"
 
-void AssignExpression::analyze(Scope* scope) {}
+void AssignExpression::analyze(Scope* scope) {
+    // @TODO: Check mutability
+    auto variable_expr = dynamic_cast<VariableExpression*>(left.get());
+    if (!variable_expr) {
+        error::add_semantic_error("Left of assign is not variable", source_ref);
+    } else {
+        auto decl = scope->get_variable(variable_expr->identifier.value);
+
+        if (!decl->is_mutable) {
+            error::add_semantic_error("Assign to non-mutable variable",
+                                      source_ref);
+        }
+    }
+
+    BinaryExpression::analyze(scope);
+}
