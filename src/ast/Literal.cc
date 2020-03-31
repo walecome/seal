d8d4aa2bae9e34b405beb96af7a1147bd762d929
Alexander Wilkens <alexander.wilkens96@gmail.com>
2020-03-31 17:22:54 +0200

    Finish CMake cleanup

--- src/ast/Literal.cc
+++ src/ast/Literal.cc
@@ -1,7 +1,10 @@
 #include "Literal.hh"
 
+#include "fmt/format.h"
+
 std::string Literal::dump(unsigned indent) const {
     std::ostringstream oss {};
-    oss << util::indent(indent) << "{" << name() << ", " << value.value << "}";
+    oss << util::indent(indent) << "{" << name() << "}";
+
     return oss.str();
 }
