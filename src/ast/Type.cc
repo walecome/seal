4f75ca3073703674fe051e03f59077764542ae5f
Alexander Wilkens <alexander.wilkens96@gmail.com>
2021-12-04 14:43:20 +0100

    Improve Type::to_string()

--- src/ast/Type.cc
+++ src/ast/Type.cc
@@ -1,6 +1,44 @@
+#include <fmt/format.h>
+
 #include "Type.hh"
 #include "Expression.hh"
 #include "Operator.hh"
+
+namespace {
+std::string to_debug_string(Primitive primitive) {
+    switch (primitive) {
+        case Primitive::STRING:
+            return "STRING";
+        case Primitive::INT:
+            return "INT";
+        case Primitive::FLOAT:
+            return "FLOAT";
+        case Primitive::VOID:
+            return "VOID";
+        case Primitive::BOOL:
+            return "BOOL";
+        case Primitive::DONT_CARE:
+            return "DONT_CARE";
+        case Primitive::INFERRED:
+            return "INFERRED";
+    }
+    ASSERT_NOT_REACHED();
+}
+
+std::string to_debug_string(Kind kind) {
+    switch (kind) {
+        case Kind::PRIMITIVE:
+            return "PRIMITIVE";
+        case Kind::ARRAY:
+            return "ARRAY";
+        case Kind::STRUCT:
+            return "STRUCT";
+        case Kind::NONE:
+            return "NONE";
+    }
+    ASSERT_NOT_REACHED();
+}
+}
 
 Primitive TypeUtil::from_string(const std::string_view s) {
     auto it = string_to_type.find(s);
@@ -9,7 +47,17 @@ Primitive TypeUtil::from_string(const std::string_view s) {
 }
 
 bool Type::operator==(const Type &other) const {
-    return m_primitive == other.m_primitive && m_kind == other.m_kind;
+    bool check_kind = m_primitive != Primitive::DONT_CARE && other.m_primitive != Primitive::DONT_CARE;
+
+    if (m_primitive != other.m_primitive) {
+        return false;
+    }
+
+    if (check_kind) {
+        return m_kind == other.m_kind;
+    }
+
+    return true;
 }
 
 bool Type::operator!=(const Type &other) const {
@@ -39,17 +87,7 @@ std::string Type::dump(unsigned) const {
 }
 
 std::string Type::to_string() const {
-    for (auto &x : TypeUtil::string_to_type) {
-        if (x.second == m_primitive) {
-            return std::string(x.first);
-        }
-    }
-
-    if (m_primitive == Primitive::INFERRED) {
-        return "INFERRED";
-    }
-
-    ASSERT_NOT_REACHED();
+    return fmt::format("{{ Primitive::{}, Kind::{} }}", to_debug_string(m_primitive), to_debug_string(m_kind));
 }
 
 std::string Type::to_user_string() const {
