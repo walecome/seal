592c3336f7f80b2856ed23108e0985064b36ca0b
Alexander Wilkens <alexander.wilkens96@gmail.com>
2022-05-18 22:48:31 +0200

    Add Type to ValuePool

--- src/common/PoolEntry.hh
+++ src/common/PoolEntry.hh
@@ -3,9 +3,12 @@
 #include <cstddef>
 
 struct PoolEntry {
+    PoolEntry();
+
     static PoolEntry create_constant(size_t key);
     static PoolEntry create_dynamic(size_t key);
 
+    // TODO: Maybe move ValuePool::Type here, and switch that that from bool.
     const size_t key;
     const bool constant;
 
