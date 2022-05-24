ae3045232c4695c36a1591a23049e7cd0b88a17c
Alexander Wilkens <alexander.wilkens96@gmail.com>
2022-05-24 22:07:57 +0200

    Add support for registering temporary crash handlers

--- src/CrashHelper.hh
+++ src/CrashHelper.hh
@@ -1,14 +1,28 @@
 #pragma once
 
 #include <string_view>
+#include <functional>
+#include <stack>
 
 class CrashHelper {
   public:
+    using crash_handler_callback_t = std::function<void()>;
+
+    class ScopedHandler {
+      public:
+        explicit ScopedHandler(crash_handler_callback_t callback);
+        ~ScopedHandler();
+    };
+
     static CrashHelper* the();
+
+    void register_crash_handler(crash_handler_callback_t callback);
+    void unregister_latest_handler();
 
   [[noreturn]] void check_crash_handler_and_terminate() const;
 
   private:
     CrashHelper();
 
+    std::stack<crash_handler_callback_t> m_crash_handlers;
 };
