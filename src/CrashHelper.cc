5df584062af4f088c11846c550bebddf43f16300
Alexander Wilkens <alexander.wilkens96@gmail.com>
2022-05-24 22:07:57 +0200

    Add support for registering temporary crash handlers

--- src/CrashHelper.cc
+++ src/CrashHelper.cc
@@ -1,7 +1,17 @@
 #include "CrashHelper.hh"
 
+#include "fmt/format.h"
+
 namespace {
   static CrashHelper* static_instance = nullptr;
+}
+
+CrashHelper::ScopedHandler::ScopedHandler(CrashHelper::crash_handler_callback_t callback) {
+  CrashHelper::the()->register_crash_handler(callback);
+}
+
+CrashHelper::ScopedHandler::~ScopedHandler() {
+  CrashHelper::the()->unregister_latest_handler();
 }
 
 CrashHelper* CrashHelper::the() {
@@ -13,6 +23,21 @@ CrashHelper* CrashHelper::the() {
 
 CrashHelper::CrashHelper() = default;
 
-void CrashHelper::check_crash_handler_and_terminate() const {
+void CrashHelper::register_crash_handler(crash_handler_callback_t callback) {
+  m_crash_handlers.push(callback);
+}
 
+void CrashHelper::unregister_latest_handler() {
+  if (m_crash_handlers.empty()) {
+    fmt::print("No crash handlers registered at call to CrashHelper::unregister_latest_handler()");
+    exit(1);
+  }
+  m_crash_handlers.pop();
+}
+
+void CrashHelper::check_crash_handler_and_terminate() const {
+  if (!m_crash_handlers.empty()) {
+    m_crash_handlers.top()();
+  }
+  exit(1);
 }
