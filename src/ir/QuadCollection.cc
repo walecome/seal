e5aea327f7fe5e48a5a946f7c8bcc7dfe82536f4
Alexander Wilkens <alexander.wilkens96@gmail.com>
2022-05-30 21:44:25 +0200

    Improve pretty printing of quads

--- src/ir/QuadCollection.cc
+++ src/ir/QuadCollection.cc
@@ -3,25 +3,41 @@
 #include "ir/QuadCollection.hh"
 
 void QuadCollection::dump() const {
-    constexpr int width = 80;
+    constexpr int width = 120;
 
-    constexpr int col_width = 16;
-
-    auto print_row = [col_width](
-                         const std::string_view a, const std::string_view b,
-                         const std::string_view c, const std::string_view d,
-                         const std::string e) {
-        fmt::print("|{:<{}}", a, col_width - 6);
-        fmt::print("|{:<{}}", b, col_width);
-        fmt::print("|{:<{}}", c, col_width);
-        fmt::print("|{:<{}}", d, col_width);
-        fmt::print("|{:<{}}", e, col_width);
+    auto print_row = [](const std::string_view a, const std::string_view b,
+                        const std::string_view c, const std::string_view d,
+                        const std::string e, const std::string f) {
+        // -2 for '|' and space.
+        int col_width = 120 / 6 - 2;
+        fmt::print("|{:>{}} ", a, col_width);
+        fmt::print("|{:>{}} ", b, col_width);
+        fmt::print("|{:>{}} ", c, col_width);
+        fmt::print("|{:>{}} ", d, col_width);
+        fmt::print("|{:>{}} ", e, col_width);
+        fmt::print("|{:>{}} ", f, col_width);
         fmt::print("|\n");
     };
 
-    fmt::print("{:-^{}}\n", "", width);
-    print_row("ATTRIBUTES", "OPCODE", "DEST", "SRC_A", "SRC_B");
-    fmt::print("{:-^{}}\n", "", width);
+    auto print_separator = [] { fmt::print("{:-^{}}\n", "", width + 1); };
+
+    auto print_column_header = [print_row, print_separator] {
+        print_separator();
+        print_row("ADDRESS", "META", "OPCODE", "DEST", "SRC_A", "SRC_B");
+        print_separator();
+    };
+
+    auto print_function_header = [print_column_header, print_separator](
+                                     auto function_id, bool first) {
+        if (!first) {
+            print_separator();
+            fmt::print("\n");
+        }
+        print_separator();
+        std::string content = fmt::format("Function #{}", function_id);
+        fmt::print("|{:^{}}|\n", content, width - 1);
+        print_column_header();
+    };
 
     std::map<unsigned, unsigned> quad_to_function {};
 
@@ -29,14 +45,19 @@ void QuadCollection::dump() const {
         quad_to_function.insert({ quad_idx, function_id });
     }
 
+    bool first_function_printed = false;
     for (unsigned i = 0; i < quads.size(); ++i) {
         auto [labels, opcode, dest, src_a, src_b] =
             quads[i].to_string_segments();
 
-        if (quad_to_function.count(i)) {
-            labels = fmt::format("F#{} {}", quad_to_function[i], labels);
+        bool new_function = quad_to_function.find(i) != quad_to_function.end();
+        if (new_function) {
+            print_function_header(quad_to_function[i], !first_function_printed);
+            first_function_printed = true;
         }
-        print_row(labels, opcode, dest, src_a, src_b);
+
+        std::string address = fmt::format("{0:#x}", i);
+        print_row(address, labels, opcode, dest, src_a, src_b);
     }
-    fmt::print("{:-^{}}\n", "", width);
+    print_separator();
 }
