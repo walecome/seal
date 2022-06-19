448fffd53d32be67f1bf081f6f7479eb761056f3
Alexander Wilkens <alexander.wilkens96@gmail.com>
2022-06-19 16:50:33 +0200

    Implement Array::add_element

--- src/builtin/Array.cc
+++ src/builtin/Array.cc
@@ -21,14 +21,13 @@ Value create_array(const std::vector<Value>& args) {
 }
 
 Value add_element(const std::vector<Value>& args) {
-    ASSERT_NOT_REACHED_MSG("BuiltIn::add_element not implemented");
-    // ASSERT(args.size() == 2);
-    // Value& vec = context.get_value(args[0]);
-    // ASSERT(vec.is_vector());
+    ASSERT(args.size() == 2);
 
-    // vec.as_vector().add(args[1]);
+    Value vec = args[0];
+    ASSERT(vec.is_vector());
+    vec.as_vector().add(args[1]);
 
-    // return context.dynamic_pool().create_integer(vec.as_vector().length());
+    return Value::create_integer(vec.as_vector().length());
 }
 
 }  // namespace BuiltIn
